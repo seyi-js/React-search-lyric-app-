e4e63330f69da8a6b825b31dc366d90928542125
ADEBAYO SAMUEL OLUWASEYI <adebayosamueljahsmine30@gmail.com>
2020-04-17 21:24:24 +0100

    Added Github link and Live Demo

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
-# React Lyrics Serch App
+# React Lyrics Search App
 
 ## Built using:
 
 - React
 - React-Redux
 - Musixmatch api
+
+### Live Demo : (https://react-lyric-app.netlify.app/)
--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -4,6 +4,7 @@ import React from 'react'
     return (
         <nav className="navbar navbar-dark bg-dark mb-5">
             <span className="navbar-brand mb-8 h1 mx-auto">Search for your favourite song's lyrics</span>
+            <span className="navbar-brand mb-8 h1 mx-auto"><i className="fab fa-github"></i><a href="https://github.com/seyi-js/React-search-lyric-app-" target='_blank'>Github</a> </span>
         </nav>
     )
 }
